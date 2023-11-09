cc7ae7e95994217304d9aa5b0b421e7dcdc0edb2
Ayushi Gupta <ayushi.gupta2@globallogic.com>
2023-11-09 18:02:00 +0530

    initial commit

---
+++ src/main/java/com/ayushi/quizbasedapplication/QuizbasedapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.ayushi.quizbasedapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizbasedapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizbasedapplicationApplication.class, args);
+	}
+
+}
