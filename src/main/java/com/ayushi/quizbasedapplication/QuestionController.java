ec0b885e35690092686b0654d412c078a89b4151
Ayushi Gupta <ayushi.gupta2@globallogic.com>
2023-11-21 00:06:07 +0530

    new commit on new branch

--- src/main/java/com/ayushi/quizbasedapplication/QuestionController.java
+++ src/main/java/com/ayushi/quizbasedapplication/QuestionController.java
@@ -1,14 +1,31 @@
 package com.ayushi.quizbasedapplication;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("question")
+@RequestMapping("/question")
 public class QuestionController {
-    @GetMapping("allQuestions")
-    public String getAllQuestions() {
-        return "Hi, These are your questions";
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("/allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        return questionService.getAllQuestions();
+
+    }
+
+    @GetMapping("category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
     }
 }
