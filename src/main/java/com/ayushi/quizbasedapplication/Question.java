34ac36c7afb4daebbf6af0c4e972a7e6d3cc31d0
Ayushi Gupta <ayushi.gupta2@globallogic.com>
2023-11-27 23:35:55 +0530

    new changes

--- src/main/java/com/ayushi/quizbasedapplication/Question.java
+++ src/main/java/com/ayushi/quizbasedapplication/Question.java
@@ -19,10 +19,7 @@ public class Question {
     private String option2;
     private String option3;
     private String option4;
-    private String question_title;
-    private String right_answer;
-
-
-
+    private String questionTitle;
+    private String rightAnswer;
 
 }
